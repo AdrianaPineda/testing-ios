0e83c90c4fd4f44ce2ef7c869992e5b50d82fe3d
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-08-11 11:12:42 -0500

    Create Xcode project

---
+++ testingWidget/testingWidget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testingWidget
+//
+//  Created by Adriana Pineda on 8/11/16.
+//  Copyright © 2016 Adriana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
